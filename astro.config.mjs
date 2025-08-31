29ab6a62142a25eb95a0e1ceaafb07993d8377e3
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-08-31 10:33:07 -0500

    core: added adapter for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
 import svelte from "@astrojs/svelte";
+import cloudflare from "@astrojs/cloudflare";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	env: {
-		schema: {
-			GOOGLE_API_KEY: envField.string({access: "secret", context: "server"}),
-			PUBLIC_GOOGLE_MAPS_PLATFORM_API_KEY: envField.string({ access: "public", context: "client" }),
-		},
+  env: {
+      schema: {
+          GOOGLE_API_KEY: envField.string({access: "secret", context: "server"}),
+          PUBLIC_GOOGLE_MAPS_PLATFORM_API_KEY: envField.string({ access: "public", context: "client" }),
+      },
 	},
-	integrations: [svelte()],
+
+  integrations: [svelte()],
+  adapter: cloudflare(),
 });
