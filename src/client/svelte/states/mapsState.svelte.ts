7e07da84f38711d3088401137a18262601ee8c50
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-08-31 11:20:19 -0500

    core: added wrangler json file

--- src/client/svelte/states/mapsState.svelte.ts
+++ src/client/svelte/states/mapsState.svelte.ts
@@ -3,5 +3,4 @@ import { CoordinateClick } from "~/common/classes/CoordinateClick.svelte";
 export const mapsState = $state({
     clicks: [] as CoordinateClick[],
     markers: [] as { marker: google.maps.marker.AdvancedMarkerElement; pin: google.maps.marker.PinElement }[],
-    selected: undefined as CoordinateClick | undefined,
 });
